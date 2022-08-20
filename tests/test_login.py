7f08fbda88dbdbf8bcde6c4788a1fa2ba0b4b375
Javan Moe <javan.moe@mindlinksoft.com>
2022-08-20 22:07:51 +0100

    Fixing tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,21 +10,18 @@ class TestLogin(BaseClass):
         checkout_info_page = basket_page.click_checkout()
         checkout_overview_page = checkout_info_page.enter_details().click_continue()
         checkout_overview_page.click_finish()
-        self.helpers.wait_for_element_to_be_visible()
 
     def test_product_sorting_by_name(self, login_as_valid_user):
-        home_page = login_as_valid_user.enter_credentials()
-        item1 = home_page.first_item_name()
-        home_page.sort_items_by_name('z-a')
-        item2 = home_page.first_item_name()
-        assert home_page.compare_item_name(item1, item2)
+        item1 = login_as_valid_user.first_item_name()
+        login_as_valid_user.sort_items_by_name('z-a')
+        item2 = login_as_valid_user.first_item_name()
+        assert login_as_valid_user.compare_item_name(item1, item2)
 
     def test_product_sort_by_price(self, login_as_valid_user):
-        home_page = login_as_valid_user.enter_credentials()
-        price1 = home_page.first_item_price()
-        home_page.sort_items_by_price('low-high')
-        price2 = home_page.first_item_price()
-        assert home_page.compare_item_price(price1, price2)
+        price1 = login_as_valid_user.first_item_price()
+        login_as_valid_user.sort_items_by_price('low-high')
+        price2 = login_as_valid_user.first_item_price()
+        assert login_as_valid_user.compare_item_price(price1, price2)
 
 
 if __name__ == '__main__':
